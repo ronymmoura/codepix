c70124fc4bd5efda22e16b2dcc2a3ed310bb493a
Rony Moura <ronymmoura@gmail.com>
2023-10-21 21:06:40 -0300

    Add models

---
+++ codepix/domain/model/transaction.go
@@ -0,0 +1,104 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+const (
+	TransactionPending   string = "pending"
+	TransactionCompleted string = "completed"
+	TransactionError     string = "error"
+	TransactionConfirmed string = "confirmed"
+)
+
+type TransactionRepositoryInterface interface {
+	Register(transaction *Transaction) error
+	Save(transaction *Transaction) error
+	Find(ind string) (*Transaction, error)
+}
+
+type Transactions struct {
+	Transaction []Transaction
+}
+
+type Transaction struct {
+	Base              `valid:"required"`
+	AccountFrom       *Account `valid:"-"`
+	Amount            float64  `json:"amount" valid:"notnull"`
+	PixKeyTo          *PixKey  `valid:"-"`
+	Status            string   `json:"status" valid:"notnull"`
+	Description       string   `json:"description" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
+
+func (transaction *Transaction) isValid() error {
+	_, err := govalidator.ValidateStruct(transaction)
+
+	if transaction.Amount <= 0 {
+		return errors.New("the amount must be greater than 0")
+	}
+
+	if transaction.Status != TransactionPending && transaction.Status != TransactionCompleted && transaction.Status != TransactionError {
+		return errors.New("invalid status for the transaction")
+	}
+
+	if transaction.PixKeyTo.AccountID == transaction.AccountFrom.ID {
+		return errors.New("the source and destination account cannot be the same")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string, id string) (*Transaction, error) {
+	transaction := Transaction{
+		AccountFrom: accountFrom,
+		Amount:      amount,
+		PixKeyTo:    pixKeyTo,
+		Status:      TransactionPending,
+		Description: description,
+	}
+
+	if id == "" {
+		transaction.ID = uuid.NewV4().String()
+	} else {
+		transaction.ID = id
+	}
+
+	transaction.CreatedAt = time.Now()
+	err := transaction.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &transaction, nil
+}
+
+func (t *Transaction) Complete() error {
+	t.Status = TransactionCompleted
+	t.UpdatedAt = time.Now()
+	err := t.isValid()
+	return err
+}
+
+func (t *Transaction) Confirm() error {
+	t.Status = TransactionConfirmed
+	t.UpdatedAt = time.Now()
+	err := t.isValid()
+	return err
+}
+
+func (t *Transaction) Cancel() error {
+	t.Status = TransactionError
+	t.UpdatedAt = time.Now()
+	err := t.isValid()
+	return err
+}
---
+++ codepix/go.mod
@@ -0,0 +1,10 @@
+module github.com/ronymmoura/codepix
+
+go 1.18
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/satori/go.uuid v1.2.0
+)
+
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
